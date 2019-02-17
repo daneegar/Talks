9875f77d03ac07d512c46eff952cbd6bf6618ad3
Denis Garifyanov <daneegar@gmail.com>
2019-02-17 12:40:20 +0300

    Причесал немного, функционал не добавлялся.

--- Talks/ProfileViewController.swift
+++ Talks/ProfileViewController.swift
@@ -21,21 +21,16 @@ class ProfileViewController: UIViewController {
 
     
     
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)   {
-        print("init nibName style")
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
+    //MARK: - переопределенный конструктор
     required init?(coder aDecoder: NSCoder) {
         print(self.buttonEdit?.frame ?? "There is no button yet")
-        //TODO: - объяснить в чем проблема:
-        //Если обращаться к свойству экземпляра класса в конструкторе, то нужно понимать когда это конкретное свойство инициализируется.
-        //в случае с атулетами, они по умолчанию weak, эти сслыки удерживаются иерархией вью, видимо, когда класс вью инициализируется он еще не встроен в иерархию, отсюда и отсутсвие ссылки на объект.
+//        TODO: - объяснить в чем проблема:
+//        Если обращаться к свойству экземпляра класса в конструкторе, то нужно понимать когда это конкретное свойство инициализируется.
+//        в случае с атулетами, они по умолчанию weak, эти сслыки удерживаются иерархией вью, видимо, когда класс вью инициализируется он еще не встроен в иерархию, отсюда и отсутсвие ссылки на объект.
         super.init(coder: aDecoder)
     }
     
-    
+    //MARK: - методы вызываемые в жизненом цикле вью
     override func viewDidLoad() {
         self.threadLogger.printStep()
         super.viewDidLoad()
@@ -43,8 +38,6 @@ class ProfileViewController: UIViewController {
         self.setupButton()
         self.setupProfilePhotoImageAndAddButton()
     }
-    
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.threadLogger.printStep()
@@ -53,7 +46,8 @@ class ProfileViewController: UIViewController {
         super.viewDidAppear(animated)
         self.threadLogger.printStep()
         print("The button frame is: \(self.buttonEdit.frame)")
-        //TODO: - так как сториборд мы верстаем в превью iPhone SE то в первом приближении устанавливаются значения Frame исходя из того как и где верстали, при включении в иерархию вьюшек применяются консстрейны и исходя из них перерасчитываются параметры frame свойста кнопки.
+//                TODO: - объяснить почему значения поменялись:
+//            - так как сториборд мы верстаем в превью iPhone SE то в первом приближении устанавливаются значения Frame исходя из того как и где верстали, при включении в иерархию вьюшек применяются консстрейны и исходя из них перерасчитываются параметры frame свойста кнопки.
     }
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
